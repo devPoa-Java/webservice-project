9736cec31604e9869722c9ef271b1154e2d05574
Sandro M. dos Santos <sandromarista@gmail.com>
2025-07-09 12:50:32 -0300

    User entity and resource

---
+++ src/main/java/com/pessoal/webservices/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.pessoal.webservices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pessoal.webservices.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "Maria", "maria@gmail.com", "5199955558", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
