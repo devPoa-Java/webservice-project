613961776b57d142132e4046bba92b848421a848
Sandro M. dos Santos <sandromarista@gmail.com>
2025-07-10 12:57:06 -0300

    Service layer, component registration

---
+++ src/main/java/com/pessoal/webservices/services/UserService.java
@@ -0,0 +1,29 @@
+package com.pessoal.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pessoal.webservices.entities.User;
+import com.pessoal.webservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		
+		return obj.get();
+	}
+
+}
