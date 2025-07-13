ef25778c22529928461ef3d304b43fab9c3f1f00
Sandro M. dos Santos <sandromarista@gmail.com>
2025-07-13 14:20:26 -0300

    Ajuste para ambiente de prod

--- src/main/java/com/pessoal/webservices/config/TestConfig.java
+++ src/main/java/com/pessoal/webservices/config/TestConfig.java
@@ -22,7 +22,7 @@ import com.pessoal.webservices.repositories.ProductRepository;
 import com.pessoal.webservices.repositories.UserRepository;
 
 @Configuration
-@Profile("dev")
+@Profile("prod")
 public class TestConfig implements CommandLineRunner {
 
     private final ProductRepository productRepository;
